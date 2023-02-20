ce4c220b721c40ffb157ada0757c9fa25840d2f6
raymond <raymondtemtsen@gmail.com>
2023-02-20 23:51:57 +0100

    testing

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">AboutHim</RouterLink>
       </nav>
     </div>
   </header>
@@ -27,22 +27,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
 
 nav a.router-link-exact-active {
   color: var(--color-text);
